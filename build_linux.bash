5c57ebb9cae9dc074a4bc0e72e5f21dad0640af0
drlotus <drlotus@users.noreply.github.com>
2024-07-25 12:59:42 +0300

    Update build_linux.bash

--- build_linux.bash
+++ build_linux.bash
@@ -1,4 +1,4 @@
 export CMAKE_PREFIX_PATH=/benchmark/build-benchmark:$CMAKE_PREFIX_PATH
 
 cmake -S . -B build
-cmake --build -Dbenchmark_DIR=/benchmark/build-benchmark build
+cmake -Dbenchmark_DIR=/benchmark/build-benchmark --build build
